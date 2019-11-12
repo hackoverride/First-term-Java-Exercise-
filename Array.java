fbd0618a4fc7623f534aec662e581b6f3c56c2d2
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-12 06:45:09 +0100

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,15 @@
+/* 
+*   I go through all Java array aspects I can think of
+*/
+
+//Ways to start an array:
+
+int[] numbers = {12, 15, 32, 12, 02, 33, 123, 445, 120, 32}; //Declare and add known value
+String[] names; // I initiate an array but dont start it. 
+String[] cars = new String[n] //n is the set length of the array.
+boolean[][] xx = {{true, false, true}{false, true, false}}; // multi dim. array...
+
+/* DEFAULT INITIAL VALUE=
+*   numbers are 0, booleans are false and objects like Strings are null...
+*/
+
