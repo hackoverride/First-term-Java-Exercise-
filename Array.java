01c707343b5950855b37a4e21456f9db5a128823
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-13 14:03:27 +0100

    Update Array.java

--- Array.java
+++ Array.java
@@ -8,8 +8,12 @@ int[] numbers = {12, 15, 32, 12, 02, 33, 123, 445, 120, 32}; //Declare and add k
 String[] names; // I initiate an array but dont start it. 
 String[] cars = new String[n] //n is the set length of the array.
 boolean[][] xx = {{true, false, true}{false, true, false}}; // multi dim. array...
+int grid[][] = {{1253, 1232, 1543},
+                {12554, 1254, 3254},
+                {3542, 12554, 32566}};
 
 /* DEFAULT INITIAL VALUE=
 *   numbers are 0, booleans are false and objects like Strings are null...
 */
+
 
