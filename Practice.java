4ebeefa5402465c458d7029495869a81049fadfb
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-15 00:12:14 +0100

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,15 @@
+import javax.util.*;
+import java.io.*;
+import java.integer.*;
+import java.swing.JOptionPane;
+
+// sjekke forskjellige imports som vi bruker normalt i timen.
+
+public class Practice{
+  
+  public static void main(String[] args){
+    System.out.println("Practice");
+  }
+
+
+}
