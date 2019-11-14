8410e6e135cbf2c26df5c6653337b4ea5f428199
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-15 00:39:01 +0100

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,7 +1,8 @@
-import javax.util.*;
-import java.io.*;
-import java.integer.*;
-import java.swing.JOptionPane;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+import static java.lang.System.*;
+import java.io.*; //inneholder File, FileWriter og PrintWriter.
+import java.util.*; //inneholder Scanner
 
 // sjekke forskjellige imports som vi bruker normalt i timen.
 
@@ -9,7 +10,40 @@ public class Practice{
   
   public static void main(String[] args){
     System.out.println("Practice");
+    
+    String innput = showInputDialog("Anngi filnavn");
+    showMessageDialog(null, "filbehandling ferdig, ok!");
+    
+    
   }
+  
+  
+  //EZgraphics metoder:
+  const int WIDTH = 400;
+  const int HEIGHT = WIDTH;
+  makeWindow("tittel", WIDTH, HEIGHT);
+  drawRectangle(startX, startY, lengdeX, lengdeY);
+  fillRectangle(startX, startY, lengdeX, lengdeY);
+  drawCircle(plassX, plassY, radius); //plasser midt-punktet
+  setFont("Arial", 20);
+  drawString("teksten", posX, posY);
+  setColor(255,255,255); // setter fargen
+  drawLine(posX, posY, endX, endY); //setter en linje.
+  
+  
+  
+  public static int antallPositiveTall(int[] tab){
+      int antall = 0;
+      for ( int i = 0; i < tab.length; i++ ){
+        if ( tab[i] > 0 ){
+          antall++;
+        }
+        return antall;
+      }
+    }
+  
+  
+  
 
 
 }
