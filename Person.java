37810ec5e87a90a59bf9647934228ea39f521540
Michael Lund <45572620+hackoverride@users.noreply.github.com>
2019-11-27 21:26:43 +0100

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,29 @@
+public class Person{
+
+  //setting variables
+  private int id = 0;
+  private String fornavn;
+  private String etternavn;
+  private int telefon;
+  private String epost;
+  private int alder;
+  
+  public Person(int id, String fornavn, String etternavn, int telefon, String epost, int alder){
+    this.id = id;
+    this.fornavn = fornavn;
+    this.etternavn = etternavn;
+    this.telefon = telefon;
+    this.epost = epost;
+    this.alder = alder;
+  }
+  
+  public String toString(){
+    String temp = "Name: " + fornavn + " " + etternavn + "\n" +
+                  "Phone: " + telefon + " " + "Email: " + epost + "\n" +
+                  "Age: " + alder + "\n";
+    return temp;
+  }
+
+  
+
+}
